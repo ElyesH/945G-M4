9bb1394470c92747d6dfc1dd8275600ef1c82d42
Elyes HAOUAS <ehaouas@noos.fr>
2018-06-29 23:14:02 +0200

    Add i945G based mainboard
    82945GC - 82801Gx - W83627EHG
    CPU:
     CPUID 0f65.Mmicrocode loaded correctly.
    DDR2:
     Supports Up to 4 DIMM.
    S3:
     working properly.
    
    Issues:
     1) It will not boot when using RAM 533MHz.
        I believe that there is a problem with time/clock or
        something related to clock when RAM 533 is used.
     2) It will not boot if channel 0 is not populated at all.
     3) HWM is not working. The FAN is running at high speed. Loud noisy.
    
    Change-Id: I5398b990c26d087afb20e4b1028a7a1cad4b3ee3
    Signed-off-by: Elyes HAOUAS <ehaouas@noos.fr>

---
+++ src/mainboard/nec/945g-m4/acpi_tables.c
@@ -0,0 +1,8 @@
+/* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <southbridge/intel/i82801gx/nvs.h>
+
+void acpi_create_gnvs(global_nvs_t *gnvs)
+{
+}
---
+++ src/mainboard/nec/945g-m4/cstates.c
@@ -0,0 +1,10 @@
+/* This file is part of the coreboot project. */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <device/device.h>
+#include <arch/x86/include/arch/acpigen.h>
+
+int get_cst_entries(acpi_cstate_t **entries)
+{
+	return 0;
+}
